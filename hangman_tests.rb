8ce70f7284735965c8d6f16cfec2650f484b244f
JWaltuch <jenna.waltuch@gmail.com>
2019-02-18 11:27:44 -0500

    Created hangman test file

---
+++ hangman_tests.rb
@@ -0,0 +1,10 @@
+require "test/unit"
+require_relative "hangman.rb"
+
+class TestGame < Test::Unit::TestCase
+
+  # def test_that_add_line_to_man_only_has_6_possible_options
+  #   assert_equal wrong_answers, 0..6
+  # end
+
+end
